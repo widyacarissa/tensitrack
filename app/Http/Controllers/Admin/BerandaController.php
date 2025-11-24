5c3d5f6f4aad1015dd29a3dd2e3250051e441c12
Osland07 <111648250+Osland07@users.noreply.github.com>
2025-11-24 17:33:45 +0700

    Update BerandaController.php

--- app/Http/Controllers/Admin/BerandaController.php
+++ app/Http/Controllers/Admin/BerandaController.php
@@ -21,9 +21,6 @@ class BerandaController extends Controller
             'jumlahTingkatRisiko' => $this->jumlahTingkatRisiko(),
             'jumlahFaktorRisiko' => $this->jumlahFaktorRisiko(),
             'jumlahDiagnosis' => $this->jumlahDiagnosis(),
-            'chartProvince' => $this->chartProvince(),
-            'chartCity' => $this->chartCity(),
-            'chartProfession' => $this->chartProfession(),
             'diagnosisTingkatRisiko' => $this->diagnosisTingkatRisiko(),
         ];
 
@@ -56,67 +53,6 @@ class BerandaController extends Controller
         $jumlahDiagnosis = Diagnosis::count();
 
         return $jumlahDiagnosis;
-    }
-
-    public function chartProvince()
-    {
-        $data = UserProfile::selectRaw('count(*) as count, province')
-            ->groupBy('province')
-            ->get()->toArray();
-        $indexProvince = new KotaProvinsiController;
-        $provinces = $indexProvince->indexProvince();
-        $provinces = json_decode(json_encode($provinces), true);
-
-        $province = [];
-        foreach ($provinces as $key => $value) {
-            $province[$value['province_id']] = [
-                'province' => $value['province'],
-            ];
-        }
-
-        $data = array_map(function ($item) use ($province) {
-            $item['province'] = $province[$item['province']]['province'] ?? null;
-
-            return $item;
-        }, $data);
-
-        return $data;
-    }
-
-    public function chartCity()
-    {
-        $data = UserProfile::selectRaw('count(*) as count, city')->groupBy('city')->get()->toArray();
-
-        $userProfileCity = array_column($data, 'city'); // Mengambil semua id dari hasil query
-        $userProfiles = UserProfile::whereIn('city', $userProfileCity)->get('province')->toArray();
-        $indexCity = new KotaProvinsiController;
-
-        $request = new HttpRequest;
-
-        $cities = [];
-        foreach ($userProfiles as $key => $value) {
-            $value['province'] = $indexCity->indexCity($request, $value['province']);
-            $value['province'] = json_decode(json_encode($value['province']), true);
-            foreach ($value['province'] as $key2 => $value2) {
-                $cities[$value2['city_id']] = [
-                    'city' => $value2['city_name'],
-                ];
-            }
-        }
-        $data = array_map(function ($item) use ($cities) {
-            $item['city'] = $cities[$item['city']]['city'] ?? null;
-
-            return $item;
-        }, $data);
-
-        return $data;
-    }
-
-    public function chartProfession()
-    {
-        $data = UserProfile::selectRaw('count(*) as count, profession')->groupBy('profession')->get()->toArray();
-
-        return $data;
     }
 
     public function diagnosisTingkatRisiko()
